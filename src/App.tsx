ec32530d3a5c413a4a78908ef2081bb7496afca7
alejoevilches <alejoevilches@gmail.com>
2024-04-12 15:44:45 -0300

    add cubreasientos component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./components/Navbar"
 import { Hero } from "./components/Hero"
 import { Services } from "./components/Services"
+import { Cubreautos } from "./components/Cubreautos"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Services />
+      <Cubreautos />
     </main>
   )
 }
--- src/components/Cubreautos.tsx
+++ src/components/Cubreautos.tsx
@@ -1,8 +1,10 @@
 export function Cubreautos(){
   return(
-    <section>
+    <section className="bg-red-950 text-white flex flex-col items-center">
+      <h4 className="text-2xl text-center px-3 py-5">Mantené tu tratamiento por mas tiempo con nuestros cubreautos</h4>
       <img src="public/cubreautos-banner.jpeg" alt="Info sobre cubreautos" />
       <img src="public/cubreautos-img.jpeg" alt="Imagen del cubreautos" />
+      <button className="my-4 w-max p-4 bg-red-600 rounded-2xl overflow-hidden">Comprar</button>
     </section>
   )
 }
